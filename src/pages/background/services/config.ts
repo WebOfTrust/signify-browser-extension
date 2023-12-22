1186b88ccc128d5cb5e1917d1dd6ff5f723f7834
Hunain Bin Sajid <hunainbinsajid@gmail.com>
2023-12-23 01:27:41 +0500

    feat: add chrome storage service

---
+++ src/pages/background/services/config.ts
@@ -0,0 +1,27 @@
+import { browserStorageService } from "@pages/background/services/browser-storage"
+
+const CONFIG_ENUMS = {
+    VENDOR_URL: "vendor-url"
+}
+
+const Config = () => {
+    const getUrl = async () => {
+        return await browserStorageService.getValue(CONFIG_ENUMS.VENDOR_URL);
+    }
+
+    const removeUrl = async () => {
+        await browserStorageService.removeKey(CONFIG_ENUMS.VENDOR_URL);
+    }
+
+    const setUrl = async (token: string) =>  {
+        await browserStorageService.setValue(CONFIG_ENUMS.VENDOR_URL, token)
+    }
+
+    return {
+        setUrl,
+        removeUrl,
+        getUrl
+    }
+}
+
+export const configService = Config()
