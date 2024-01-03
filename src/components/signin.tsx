8fe1ca294a38a70624e4afddba40b03a5dc0777c
Hunain Bin Sajid <hunainbinsajid@gmail.com>
2024-01-03 18:59:59 +0500

    feat: add signin screen validations

--- src/components/signin.tsx
+++ src/components/signin.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import logo from "@assets/img/128_keri_logo.png";
+import { isValidUrl } from "@pages/background/utils";
 
 interface ISignin {
   vendorUrl?: string;
@@ -11,9 +12,45 @@ export function Signin(props: ISignin): JSX.Element {
   const [vendorUrl, setVendorUrl] = useState(props?.vendorUrl);
   const [passcode, setPasscode] = useState("");
 
+  const [urlError, setUrlError] = useState("");
+  const [passcodeError, setPasscodeError] = useState("");
+
   useEffect(() => {
     setVendorUrl(props.vendorUrl);
   }, [props.vendorUrl]);
+
+  const onBlurUrl = () => {
+    if (!vendorUrl || !isValidUrl(vendorUrl)) {
+      setUrlError("Enter a valid url");
+    } else {
+      setUrlError("");
+    }
+  };
+
+  const onBlurPasscode = () => {
+    if (!passcode) {
+      setPasscodeError("Enter your passcode");
+    } else {
+      setPasscodeError("");
+    }
+  };
+
+  const handleConnect = () => {
+    let hasError = false;
+    if (!vendorUrl || !isValidUrl(vendorUrl)) {
+      setUrlError("Enter a valid url");
+      hasError = true;
+    }
+
+    if (!passcode) {
+      setPasscodeError("Enter your passcode");
+      hasError = true;
+    }
+
+    if (!hasError) {
+      props.handleConnect(vendorUrl, passcode);
+    }
+  };
 
   return (
     <div className="grid grid-cols-1 gap-2">
@@ -46,34 +83,42 @@ export function Signin(props: ISignin): JSX.Element {
         <input
           type="text"
           id="vendor_url"
-          className="bg-gray-50 border text-black border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+          className={`bg-gray-50 border text-black border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ${
+            urlError ? " text-red border-red" : ""
+          } `}
           placeholder="Enter Keria url"
           required
           value={vendorUrl}
           onChange={(e) => setVendorUrl(e.target.value)}
+          onBlur={onBlurUrl}
         />
+        {urlError ? <p className="text-red">{urlError}</p> : null}
       </div>
       <div className=" px-4 py-2">
         <input
           type="password"
           id="passcode"
-          className="bg-gray-50 border text-black border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+          className={`bg-gray-50 border text-black border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ${
+            passcodeError ? " text-red border-red" : ""
+          }`}
           placeholder="Enter your passcode"
           required
           value={passcode}
           onChange={(e) => setPasscode(e.target.value)}
+          onBlur={onBlurPasscode}
         />
+        {passcodeError ? <p className="text-red">{passcodeError}</p> : null}
       </div>
       <div className="flex flex-row justify-center">
         <button
           type="button"
-          onClick={() => props.handleConnect(vendorUrl, passcode)}
+          onClick={handleConnect}
           className="text-white bg-green hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2"
         >
           Connect
         </button>
       </div>
-      <div>
+      <div className="flex flex-row justify-center">
         <a href="#" className="font-medium text-blue-600 hover:underline">
           Don't have account?
         </a>
--- src/pages/background/utils.ts
+++ src/pages/background/utils.ts
@@ -3,3 +3,12 @@ export const senderIsPopup = (sender: chrome.runtime.MessageSender) => {
   const popupPath = origin + "/src/pages/popup/index.html";
   return sender.origin === origin && sender.url === popupPath;
 };
+
+export const isValidUrl = (str: string) => {
+  try {
+    new URL(str);
+    return true;
+  } catch (err) {
+    return false;
+  }
+};
