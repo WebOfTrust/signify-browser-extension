7fc38d6011cec1c418707260c8ff2b30c1948bfe
Hunain Bin Sajid <hunainbinsajid@gmail.com>
2024-01-10 02:04:24 +0500

    fix: add kerilogo to dialog with web_accessible_resource

--- src/pages/dialog/Dialog.tsx
+++ src/pages/dialog/Dialog.tsx
@@ -4,7 +4,7 @@ import { PopupPrompt } from "./popupPrompt";
 import { SigninItem } from "./signin";
 
 export default function Dialog({ isConnected, tab, signins }): JSX.Element {
-  // const logo = chrome.runtime.getURL("src/assets/img/128_keri_logo.png");
+  const logo = chrome.runtime.getURL("src/assets/img/128_keri_logo.png");
   const [showPopupPrompt, setShowPopupPrompt] = useState(false);
 
   const handleSetState = async () => {
@@ -31,6 +31,11 @@ export default function Dialog({ isConnected, tab, signins }): JSX.Element {
   return (
     <div className="absolute top-10 right-10 min-w-[300px] rounded text-center p-3 bg-white">
       <header className="items-center justify-center">
+        <div className="flex flex-row gap-x-2 mb-2">
+          <img src={logo} className="h-8" alt="logo" />
+          <p className="text-2xl font-bold text-green">Sign in with KERI</p>
+        </div>
+
         {showPopupPrompt ? (
           <PopupPrompt message="Select the KERI icon in your browser to proceed" />
         ) : null}
@@ -42,7 +47,6 @@ export default function Dialog({ isConnected, tab, signins }): JSX.Element {
 
         {signins?.length && isConnected ? (
           <>
-            <p className="text-2xl font-bold text-green">Sign in with KERI</p>
             {signins?.map((signin) => (
               <SigninItem signin={signin} />
             ))}
