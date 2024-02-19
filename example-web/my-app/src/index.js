c51d55d3b7edeb1b12df24779dec432520a3cd5f
Hunain Bin Sajid <hunainbinsajid@gmail.com>
2024-02-20 03:37:22 +0500

    feat: introduce subscription for client

--- example-web/my-app/src/index.js
+++ example-web/my-app/src/index.js
@@ -1,25 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-window.addEventListener(
-  "message",
-  (event) => {
-    // Accept messages only from same window
-    if (event.source !== window) {
-      return;
-    }
-    if (event.data.type && event.data.type === "signify-signature") {
-
-      alert("Signed headers received\n"+ JSON.stringify(event.data.data, null, 2));
-    }
-  },
-  false
-);
-
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
