fb638487d2d03c7deef8e615eaeb22e952708f5e
Hunain Bin Sajid <hunainbinsajid@gmail.com>
2024-03-15 02:45:21 +0500

    fix: add theme colors in switch component

--- src/components/ui/switch/switch.tsx
+++ src/components/ui/switch/switch.tsx
@@ -1,14 +1,16 @@
+import { styled } from "styled-components";
+
 const icon = (
   <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path
       d="M20 12C20 7.58172 16.4183 4 12 4M12 20C14.5264 20 16.7792 18.8289 18.2454 17"
-      stroke="white"
+      stroke="currentColor"
       stroke-width="1.5"
       stroke-linecap="round"
     />
     <path
       d="M4 12H14M14 12L11 9M14 12L11 15"
-      stroke="white"
+      stroke="currentColor"
       stroke-width="1.5"
       stroke-linecap="round"
       stroke-linejoin="round"
@@ -21,24 +23,38 @@ interface ISwitch {
   handleToggle: () => void;
 }
 
+const StyledSwitch = styled.button<Pick<ISwitch, "isChecked">>`
+  background-color: ${(props) => props.theme?.colors?.bodyBg};
+  border-color: ${(props) => props.theme?.colors?.primary};
+  border-width: 1px;
+  opacity: ${({ isChecked }) => (isChecked ? 1 : 0.6)};
+`;
+
+const StyledSwitchDiv = styled.div<Pick<ISwitch, "isChecked">>`
+  background-color: ${({ isChecked, theme }) =>
+    isChecked ? theme?.colors?.primary : theme?.colors?.bodyBg};
+  border: ${({ isChecked, theme }) =>
+    isChecked ? "unset" : `1px solid ${theme?.colors?.primary}`};
+  color: ${({ isChecked, theme }) =>
+    isChecked ? theme?.colors?.bodyBg : theme?.colors?.bodyColor};
+`;
+
 export function Switch({ handleToggle, isChecked }: ISwitch): JSX.Element {
   return (
-    <button
-      className={`w-12 h-6 rounded-full flex items-center transition duration-300 focus:outline-none shadow bg-gray-light ${
-        isChecked ? "border border-green" : " "
-      }`}
+    <StyledSwitch
+      isChecked={isChecked}
+      className={`w-12 h-6 rounded-full flex items-center transition duration-300 focus:outline-none shadow`}
       onClick={handleToggle}
     >
-      <div
+      <StyledSwitchDiv
         id="switch-toggle"
-        className={` w-6 h-6 relative rounded-full transition duration-500 transform p-1 text-white ${
-          isChecked
-            ? "bg-green translate-x-full"
-            : " bg-gray-light -translate-x-1"
+        isChecked={isChecked}
+        className={` w-6 h-6 relative rounded-full transition duration-500 transform p-1 ${
+          isChecked ? "translate-x-full" : " -translate-x-1"
         }`}
       >
         {icon}
-      </div>
-    </button>
+      </StyledSwitchDiv>
+    </StyledSwitch>
   );
 }
